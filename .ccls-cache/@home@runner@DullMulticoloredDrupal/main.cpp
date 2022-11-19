1946781455ec789989dce6125a9b8c5560a0d3dd
sHinojosa <110847155+smhc23@users.noreply.github.com>
2022-11-19 16:24:38 +0000

    XOR encryption and decryption for single char key added

--- .ccls-cache/@home@runner@DullMulticoloredDrupal/main.cpp
+++ .ccls-cache/@home@runner@DullMulticoloredDrupal/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+using namespace std;
+
+string encryptDecrypt(string toEncrypt) {
+    char key = 'K'; //Any char will work
+    string output = toEncrypt;
+    
+    for (int i = 0; i < toEncrypt.size(); i++)
+        output[i] = toEncrypt[i] ^ key;
+    
+    return output;
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+  string result;
+  result = encryptDecrypt("Hola");
+  cout<< result<<endl;
+  std::cout << encryptDecrypt(result);
 }
